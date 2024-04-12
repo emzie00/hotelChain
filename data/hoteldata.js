c143131b877834743443c23e21eb4fdcb6e874c6
Emma Church <emmaelizchurch@gmail.com>
2024-04-12 15:12:51 -0600

    hotel chain stuff

---
+++ data/hoteldata.js
@@ -0,0 +1,50 @@
+export const hotels = [
+    {
+        "name": "Salt Lake City",
+        "photo": "slcHotel.png",
+        "address": ["555 Main St", "Salt Lake City, UT 84111"],
+        "phone": "801-258-6000"
+    },
+    {
+        "name": "Los Angeles",
+        "photo": "laHotel.png",
+        "address": ["900 W Olympic Blvd", "Los Angeles, CA 90015"],
+        "phone": "(213) 743-8800"
+    },
+    {
+        "name": "Seattle",
+        "photo": "seattleHotel.png",
+        "address": ["99 Union St", "Seattle, WA 98101"],
+        "phone": "(206) 749-7000"
+    },
+    {
+        "name": "Las Vegas",
+        "photo": "lvHotel.png",
+        "address": ["3600 S Las Vegas Blvd", "Las Vegas, NV 89109"],
+        "phone": "(888) 987-6667"
+    },
+    {
+        "name": "Phoenix",
+        "photo": "phoenixHotel.png",
+        "address": ["2400 E Missouri Ave", "Phoenix, AZ 85016"],
+        "phone": "(602) 955-6600"
+    },
+    {
+        "name": "Denver",
+        "photo": "denverHotel.png",
+        "address": ["1111 14th St", "Denver, CO 80202"],
+        "phone": "(303) 389-3000"
+    },
+    {
+        "name": "Orlando",
+        "photo": "orlandoHotel.png",
+        "address": ["9801 International Dr", "Orlando, FL 32819"],
+        "phone": "(407) 284-1234"
+    },
+    {
+        "name": "New York City",
+        "photo": "nyHotel.png",
+        "address": ["27 Barclay St", "New York, NY 10007"],
+        "phone": "(646) 880-1999"
+    }
+]
